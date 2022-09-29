24e78040356d2c2ccf8fdfbd175184f8df751b1e
djbetamax <hugots23@gmail.com>
2022-09-28 22:08:37 -0300

    feat: add events on buttons and show the result of the input value
    Add Sin, Cos, and Tan button functionalities. By inserting a number on the input and then clicking on one of these options (Sin, Cos, Tan, and All) it just needs to click on Calculate and it will result in the necessary number

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-const sin = angle => Math.sin(angle).toFixed(2);
-const cos = angle => Math.cos(angle).toFixed(2);
-const tan = angle => Math.tan(angle).toFixed(2);
+const sinCalc = angle => Math.sin(angle).toFixed(2);
+const cosCalc = angle => Math.cos(angle).toFixed(2);
+const tanCalc = angle => Math.tan(angle).toFixed(2);
 
 const input = document.querySelector('.inputValue');
+const container = document.querySelector('.result-container');
+const p = document.createElement('p');
+container.appendChild(p);
 
 const btn = document.querySelector('.btn-submit')
-btn.addEventListener('click', function (angle) {
-  const sinBtn = document.querySelector('.sinAngle');
-  const cosBtn = document.querySelector('.cosAngle');
-  const tanBtn = document.querySelector('.tanAngle');
-  const allBtn = document.querySelector('.allAngle');
+btn.addEventListener('click', (angle, func)=> {
   angle = input.value;
   
-  if (sinBtn.checked == true) {
-    console.log(sin(angle));
+  if ( document.querySelector('.sinAngle' ).checked == true) {
+    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${sinCalc(angle)}</span>`;
+    document.querySelector('.sinAngle' ).checked = false
     input.value = '';
-  } else if (cosBtn.checked == true) {
-      console.log(cos(angle));
-    } else if (tanBtn.checked == true) {
-        console.log(tan(angle));
-      } else if (allBtn.checked == true) {
-          console.log(sin(angle));
-          console.log(cos(angle));
-          console.log(tan(angle));
-        } else { 
-            alert('CHOOSE AN ANGLE OPTION!')
-          }
+  } 
+  else if ( document.querySelector('.cosAngle' ).checked == true) {
+    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${cosCalc(angle)}</span>`;
+    document.querySelector('.cosAngle' ).checked = false
+    input.value = '';
+  } 
+  else if (document.querySelector( '.tanAngle').checked == true ) {
+    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${tanCalc(angle)}</span>`;
+    document.querySelector( '.tanAngle').checked = false
+    input.value = '';
+  } 
+  else if ( document.querySelector('.allAngle').checked == true ) {
+    p.innerHTML = `The angle <span>${angle}º</span> respectivily have: <span>${sinCalc(angle)}</span>, <span>${cosCalc(angle)}</span>, <span>${tanCalc(angle)}</span>`;
+    document.querySelector('.allAngle').checked = false 
+    input.value = '';
+  } else { 
+      alert('CHOOSE AN ANGLE OPTION!')
+    }
 })
 
