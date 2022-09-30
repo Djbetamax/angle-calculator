51bea769a7877b65825a4ed64fc01e737b1744f6
djbetamax <hugots23@gmail.com>
2022-09-29 22:07:52 -0300

    build: change variable names, and change if statement nesting

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const sinCalc = angle => Math.sin(angle).toFixed(2);
-const cosCalc = angle => Math.cos(angle).toFixed(2);
-const tanCalc = angle => Math.tan(angle).toFixed(2);
+const sinCalc = num => Math.sin(num).toFixed(2);
+const cosCalc = num => Math.cos(num).toFixed(2);
+const tanCalc = num => Math.tan(num).toFixed(2);
 
-const input = document.querySelector('.inputValue');
+const input = document.querySelector('.input');
 const container = document.querySelector('.result-container');
 const p = document.createElement('p');
 container.appendChild(p);
 
 const btn = document.querySelector('.btn-submit')
-btn.addEventListener('click', (angle, func)=> {
+btn.addEventListener('click', angle => {
   angle = input.value;
-  
-  if ( document.querySelector('.sinAngle' ).checked == true) {
-    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${sinCalc(angle)}</span>`;
-    document.querySelector('.sinAngle' ).checked = false
-    input.value = '';
-  } 
-  else if ( document.querySelector('.cosAngle' ).checked == true) {
-    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${cosCalc(angle)}</span>`;
-    document.querySelector('.cosAngle' ).checked = false
-    input.value = '';
-  } 
-  else if (document.querySelector( '.tanAngle').checked == true ) {
-    p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${tanCalc(angle)}</span>`;
-    document.querySelector( '.tanAngle').checked = false
-    input.value = '';
-  } 
-  else if ( document.querySelector('.allAngle').checked == true ) {
-    p.innerHTML = `The angle <span>${angle}º</span> respectivily have: <span>${sinCalc(angle)}</span>, <span>${cosCalc(angle)}</span>, <span>${tanCalc(angle)}</span>`;
-    document.querySelector('.allAngle').checked = false 
-    input.value = '';
-  } else { 
-      alert('CHOOSE AN ANGLE OPTION!')
-    }
+
+  if (angle !== '') {
+    if ( document.querySelector('.sinAngle' ).checked == true ) {
+      p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${sinCalc(angle)}</span>`;
+      document.querySelector('.sinAngle' ).checked = false;
+    } 
+    else if ( document.querySelector('.cosAngle' ).checked == true ) {
+      p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${cosCalc(angle)}</span>`;
+      document.querySelector('.cosAngle' ).checked = false;
+    } 
+    else if (document.querySelector( '.tanAngle').checked == true ) {
+      p.innerHTML = `The Cos of <span>${angle}º</span> is <span>${tanCalc(angle)}</span>`;
+      document.querySelector('.tanAngle').checked = false;
+    } 
+    else if ( document.querySelector('.allAngle').checked == true ) {
+      p.innerHTML = `The angle <span>${angle}º</span> respectivily is: <span>${sinCalc(angle)}</span>, <span>${cosCalc(angle)}</span>, <span>${tanCalc(angle)}</span>`;
+      document.querySelector('.allAngle').checked = false ;
+    } else { 
+        alert('CHOOSE AN ANGLE OPTION!');
+      }
+  } else {
+      alert('PUT A NUMBER ON INPUT!')
+  }
+  input.value = '';
 })
 
